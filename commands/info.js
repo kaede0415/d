ad44afd075aec6fb92e704a75d00ec9f8e53d2e0
Glitch (local-foggy-rubidium) <none>
2023-11-20 01:14:29 +0000

    📟🌗 Checkpoint ./index.js:29682831/6874 ./commands/showlist.js:29682831/2377 ./commands/info.js:29682831/699

---
+++ commands/info.js
@@ -0,0 +1,21 @@
+const { Client, Intents, MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+
+module.exports = {
+  data: {
+    name: "info",
+    description: "コマンド一覧を表示します",
+  },
+  async execute(interaction) {
+    const embed = new MessageEmbed()
+    .setTitle("HELP")
+    .setDescription("わからないことがある場合は[公式鯖](https://discord.gg/YFSUDemgPp)までどうぞ")
+    .addField(`= help =`,">>> この画面")
+    .addField(`= handle =`,">>> 対応状況パネル設置(業者向け)")
+    .addField(`= embed =`,">>> 埋め込み生成")
+    .addField(`= verify =`,">>> 認証パネル設置")
+    .addField(`= request =`,">>> idを指定して追加")
+    .addField(`= call =`,">>> 登録されている全員を追加")
+    .setColor("RANDOM")
+    await interaction.reply({ embeds: [ embed ] })
+  }
+}
---
+++ commands/showlist.js
@@ -0,0 +1,20 @@
+const { Client, Intents, MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+const axios = require('axios');
+const fs = require('fs');
+
+module.exports = {
+  data: {
+    name: "showlist",
+    description: "権限を持っている人の一覧を表示します",
+  },
+  async execute(interaction) {
+    const configData = fs.readFileSync("config.json", 'utf8');
+    const config = JSON.parse(configData);
+    const admin = config.admin_list
+    if(!admin.includes(interaction.user.id)) return interaction.reply({ content: "BOT管理者しか使えません", ephemeral: true })
+    const embed = new MessageEmbed()
+    .setTitle("ホワイトリスト:")
+    .setDescription(`>>> ${config.white_list.map(id => `<@${id}>`).join("\n")}`)
+    interaction.reply({ embeds: [ embed ] })
+  },
+};
